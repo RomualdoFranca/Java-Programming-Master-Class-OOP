0f627c11cbda5f5f4e3d312e3fb87a9fd0501e1e
RomualdoFranca <romualdo.ads2022@gmail.com>
2022-08-11 17:54:59 -0300

    validate all instaces fields

--- exerciseBankAccount/src/Main.java
+++ exerciseBankAccount/src/Main.java
@@ -2,9 +2,14 @@ public class Main {
     public static void main(String[] args) {
         BankAccount account = new BankAccount();
         account.setAccountNumber(100);
+        System.out.println("Account number = " + account.getAccountNumber());
         account.setBalance(500.0);
+        System.out.println("Balance = " + account.getBalance());
         account.setCustomerName("Mamão");
+        System.out.println("Name = " + account.getBalance());
         account.setEmail("mamao@gmail");
+        System.out.println("email =  " + account.getEmail());
         account.setPhone("81-89887671");
+        System.out.println("Phone = " + account.getPhone());
     }
 }
